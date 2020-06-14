88bacc3874423847525473c3dc9863d6279b0077
jerinnishokc <jerinnishok@outlook.com>
2020-06-15 00:06:01 +0530

    Updates

---
+++ precache-manifest.fa4ce37fe99ba54aa49a5c0539d159e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55975f6c8297db3fea7fdfb692830186",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ded7536c71fc51a6716c",
+    "url": "/monsters-rolodex/static/css/main.f0c70cdc.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ded7536c71fc51a6716c",
+    "url": "/monsters-rolodex/static/js/main.dca0c277.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
